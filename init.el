7c5e7ca02af013d0aef282e562d81313f833692c
Leonardo Soto <leo.soto@gmail.com>
2013-03-18 00:23:58 -0300

    Improvements for Rails/Git workflow

--- init.el
+++ init.el
@@ -29,6 +29,10 @@
     haml-mode
     coffee-mode
     find-things-fast
+    rinari
+    ido-better-flex
+    magit
+    mo-git-blame
     )
   "A list of packages to ensure are installed at launch.")
 
@@ -60,17 +64,86 @@
   (set 'tab-width 2))
 (add-hook 'coffee-mode-hook 'coffee-custom)
 
-; Ruby: Ident with just 2 spaces in method calls
+; Ruby:
+(require 'ruby-mode)
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+
+; Ident with just 2 spaces in method calls:
 (setq ruby-deep-indent-paren nil)
 
 ; Line numbers on the left
 (global-linum-mode t)
 
+; magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
 ; HAML
 (add-hook 'haml-mode-hook
           (lambda ()
             (setq indent-tabs-mode nil)
-            (define-key haml-mode-map "\C-m" 'newline-and-indent)))
+            (define-key haml-mode-map "\C-m" 'newline-and-indent)
+            ))
+(remove-hook 'haml-mode-hook 'turn-on-auto-fill)
+(server-start)
+
+; Rinari (Rails)
+
+(add-hook 'rinari-minor-mode-hook (lambda ()
+  (define-prefix-command 'pd-rinari-map1)
+  (define-prefix-command 'pd-rinari-map2)
+  (local-set-key (kbd "s-R") 'pd-rinari-map1)
+  (local-set-key (kbd "s-r") 'pd-rinari-map2)
+
+  (define-key pd-rinari-map1 "'" 'rinari-find-by-context)
+  (define-key pd-rinari-map1 ";" 'rinari-find-by-context)
+  (define-key pd-rinari-map1 "c" 'rinari-console)
+  (define-key pd-rinari-map1 "d" 'rinari-cap)
+  (define-key pd-rinari-map1 "e" 'rinari-insert-erb-skeleton)
+  (define-key pd-rinari-map1 "g" 'rinari-rgrep)
+  (define-key pd-rinari-map1 "p" 'rinari-goto-partial)
+  (define-key pd-rinari-map1 "q" 'rinari-sql)
+  (define-key pd-rinari-map1 "r" 'rinari-rake)
+  (define-key pd-rinari-map1 "s" 'rinari-script)
+  (define-key pd-rinari-map1 "t" 'rinari-test)
+  (define-key pd-rinari-map1 "w" 'rinari-web-server)
+  (define-key pd-rinari-map1 "x" 'rinari-extract-partial)
+
+  (define-key pd-rinari-map2 ";" 'rinari-find-by-context)
+  (define-key pd-rinari-map2 "C" 'rinari-find-cells)
+  (define-key pd-rinari-map2 "F" 'rinari-find-features)
+  (define-key pd-rinari-map2 "M" 'rinari-find-mailer)
+  (define-key pd-rinari-map2 "S" 'rinari-find-steps)
+  (define-key pd-rinari-map2 "Y" 'rinari-find-sass)
+  (define-key pd-rinari-map2 "a" 'rinari-find-application)
+  (define-key pd-rinari-map2 "c" 'rinari-find-controller)
+  (define-key pd-rinari-map2 "e" 'rinari-find-environment)
+  (define-key pd-rinari-map2 "f" 'rinari-find-file-in-project)
+  (define-key pd-rinari-map2 "h" 'rinari-find-helper)
+  (define-key pd-rinari-map2 "i" 'rinari-find-migration)
+  (define-key pd-rinari-map2 "j" 'rinari-find-javascript)
+  (define-key pd-rinari-map2 "l" 'rinari-find-lib)
+  (define-key pd-rinari-map2 "m" 'rinari-find-model)
+  (define-key pd-rinari-map2 "n" 'rinari-find-configuration)
+  (define-key pd-rinari-map2 "o" 'rinari-find-log)
+  (define-key pd-rinari-map2 "p" 'rinari-find-public)
+  (define-key pd-rinari-map2 "r" 'rinari-find-rspec)
+  (define-key pd-rinari-map2 "s" 'rinari-find-script)
+  (define-key pd-rinari-map2 "t" 'rinari-find-test)
+  (define-key pd-rinari-map2 "u" 'rinari-find-plugin)
+  (define-key pd-rinari-map2 "v" 'rinari-find-view)
+  (define-key pd-rinari-map2 "w" 'rinari-find-worker)
+  (define-key pd-rinari-map2 "x" 'rinari-find-fixture)
+  (define-key pd-rinari-map2 "y" 'rinari-find-stylesheet)
+  (define-key pd-rinari-map2 "z" 'rinari-find-rspec-fixture)
+  ))
+
+(add-hook 'haml-mode-hook 'rinari-minor-mode)
+
+(ido-better-flex/enable)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
